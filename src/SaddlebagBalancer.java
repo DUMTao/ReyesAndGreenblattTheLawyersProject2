fd2d06a219676dcb7dd62476ad712d02e3f99e1a
DUMTao <kreyes@mocs.flsouthern.edu>
2026-02-13 14:56:14 -0500

    Initial Commit :)))

---
+++ src/SaddlebagBalancer.java
@@ -0,0 +1,22 @@
+/**
+ * Returns the authors' names.
+ * @return  Kamil Reyes and Greenblatt
+ */
+
+import java.util.*;
+
+public class SaddlebagBalancer {
+
+    //Base Cases when it's greater, sum is greater or when the sum if less than target
+
+
+
+    //Takes a list of integers that if the weight of the packages that returns the solution list
+    public static List<Integer> getPartition(List<Integer> packageWeights){
+
+    }
+
+    public static String getAuthors(){
+        return "Kamil Reyes and Matt Greenblatt";
+    }
+}
